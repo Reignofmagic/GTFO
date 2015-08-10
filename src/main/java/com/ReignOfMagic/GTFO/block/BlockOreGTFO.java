2983ac21e9bfffd39700c2d759c1da95772aaec5
ReignOfMagic <nelriana@gmail.com>
2015-08-09 22:42:20 -0500

    Removed Eclipse Dirrectory
    (this is where minecraft is stored for me)

---
+++ src/main/java/com/ReignOfMagic/GTFO/block/BlockOreGTFO.java
@@ -0,0 +1,7 @@
+package com.ReignOfMagic.gtfo.block;
+
+/**
+ * Created by Chris on 8/9/2015.
+ */
+public class BlockOreGTFO {
+}
---
+++ src/main/java/com/ReignOfMagic/GTFO/item/ItemGemTopaz.java
@@ -0,0 +1,20 @@
+package com.ReignOfMagic.gtfo.item;
+
+
+public class ItemGemTopaz extends ItemGTFO
+{
+    public ItemGemTopaz()
+    {
+        super();
+        this.setUnlocalizedName("gemCitrine");
+
+
+
+
+
+    }
+
+
+
+
+}
